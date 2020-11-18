4c787c23771c4bf6140076bc10d2bb007ac430b5
Lameck1 <lameck72@gmail.com>
2020-11-18 03:39:52 +0300

    Add dummy code for test file `too_long.rb`
    - Added repeated lines of code for testing length of file

---
+++ test_files/too_long.rb
@@ -0,0 +1,219 @@
+class Addition
+    def initialize(*_args)
+      @add = proc { |args| args.reduce(:+) }
+    end
+  
+    def add(*args)
+      @add.call(args)
+    end
+  end
+  # bad commment
+  class Addition
+    def initialize(*_args)
+      @add = proc { |args| args.reduce(:+) }
+    end
+  
+    def add(*args)
+      @add.call(args)
+    end
+  end
+  # bad commment
+  
+  class Addition
+    def initialize(*_args)
+      @add = proc { |args| args.reduce(:+) }
+    end
+  
+    def add(*args)
+      @add.call(args)
+    end
+  end
+  # bad commment
+  
+  class Addition
+    def initialize(*_args)
+      @add = proc { |args| args.reduce(:+) }
+    end
+  
+    def add(*args)
+      @add.call(args)
+    end
+  end
+  # bad commment
+  
+  class Addition
+    def initialize(*_args)
+      @add = proc { |args| args.reduce(:+) }
+    end
+  
+    def add(*args)
+      @add.call(args)
+    end
+  end
+  # bad commment
+  
+  class Addition
+    def initialize(*_args)
+      @add = proc { |args| args.reduce(:+) }
+    end
+  
+    def add(*args)
+      @add.call(args)
+    end
+  end
+  # bad commment
+  
+  class Addition
+    def initialize(*_args)
+      @add = proc { |args| args.reduce(:+) }
+    end
+  
+    def add(*args)
+      @add.call(args)
+    end
+  end
+  # bad commment
+  
+  class Addition
+    def initialize(*_args)
+      @add = proc { |args| args.reduce(:+) }
+    end
+  
+    def add(*args)
+      @add.call(args)
+    end
+  end
+  # bad commment
+  
+  class Addition
+    def initialize(*_args)
+      @add = proc { |args| args.reduce(:+) }
+    end
+  
+    def add(*args)
+      @add.call(args)
+    end
+  end
+  # bad commment
+  
+  class Addition
+    def initialize(*_args)
+      @add = proc { |args| args.reduce(:+) }
+    end
+  
+    def add(*args)
+      @add.call(args)
+    end
+  end
+  # bad commment
+  
+  class Addition
+    def initialize(*_args)
+      @add = proc { |args| args.reduce(:+) }
+    end
+  
+    def add(*args)
+      @add.call(args)
+    end
+  end
+  # bad commment
+  
+  class Addition
+    def initialize(*_args)
+      @add = proc { |args| args.reduce(:+) }
+    end
+  
+    def add(*args)
+      @add.call(args)
+    end
+  end
+  # bad commment
+  
+  class Addition
+    def initialize(*_args)
+      @add = proc { |args| args.reduce(:+) }
+    end
+  
+    def add(*args)
+      @add.call(args)
+    end
+  end
+  # bad commment
+  
+  class Addition
+    def initialize(*_args)
+      @add = proc { |args| args.reduce(:+) }
+    end
+  
+    def add(*args)
+      @add.call(args)
+    end
+  end
+  # bad commment
+  
+  class Addition
+    def initialize(*_args)
+      @add = proc { |args| args.reduce(:+) }
+    end
+  
+    def add(*args)
+      @add.call(args)
+    end
+  end
+  # bad commment
+  
+  class Addition
+    def initialize(*_args)
+      @add = proc { |args| args.reduce(:+) }
+    end
+  
+    def add(*args)
+      @add.call(args)
+    end
+  end
+  # bad commment
+  
+  class Addition
+    def initialize(*_args)
+      @add = proc { |args| args.reduce(:+) }
+    end
+  
+    def add(*args)
+      @add.call(args)
+    end
+  end
+  # bad commment
+  
+  class Addition
+    def initialize(*_args)
+      @add = proc { |args| args.reduce(:+) }
+    end
+  
+    def add(*args)
+      @add.call(args)
+    end
+  end
+  # bad commment
+  
+  class Addition
+    def initialize(*_args)
+      @add = proc { |args| args.reduce(:+) }
+    end
+  
+    def add(*args)
+      @add.call(args)
+    end
+  end
+  # bad commment
+  
+  class Addition
+    def initialize(*_args)
+      @add = proc { |args| args.reduce(:+) }
+    end
+  
+    def add(*args)
+      @add.call(args)
+    end
+  end
+  # bad commment
+  
