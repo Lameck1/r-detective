ab1b239c9634eeca7458f6ba9136e81e359f3fc3
Lameck1 <lameck72@gmail.com>
2020-11-18 03:47:05 +0300

    Add error file content
    - Added intetionally broken code for testing code errors

---
+++ test_files/code_smells.rb
@@ -0,0 +1,13 @@
+
+class  Addition
+    def initialize(*_args)
+      @add = Proc.new { |args| args.reduce(:+) }
+    end
+  
+    def add(*args)
+      @add.call(args)  
+    end
+  end
+  #bad commment
+  
+  
